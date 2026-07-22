89912b8730c4bd1ccbea28da48156243dc094899
Jonathan Johnson <jon.johnson@ucsf.edu>
2026-07-21 21:58:26 -0700

    Use Fetch Service to Search

--- packages/ilios-common/addon/services/search.js
+++ packages/ilios-common/addon/services/search.js
@@ -1,26 +1,7 @@
 import Service, { service } from '@ember/service';
-import { waitForFetch } from '@ember/test-waiters';
 
 export default class SearchService extends Service {
-  @service iliosConfig;
-  @service session;
-
-  get authHeaders() {
-    const session = this.session;
-    const { jwt } = session.data.authenticated;
-    const headers = {};
-    if (jwt) {
-      headers['X-JWT-Authorization'] = `Token ${jwt}`;
-    }
-
-    return new Headers(headers);
-  }
-
-  get host() {
-    return this.iliosConfig.apiHost
-      ? this.iliosConfig.apiHost
-      : window.location.protocol + '//' + window.location.host;
-  }
+  @service fetch;
 
   /**
    * Find courses
@@ -28,13 +9,9 @@ export default class SearchService extends Service {
   async forCurriculum(q, size, from, schools, years) {
     const schoolQuery = schools ? `&schools=${schools.join('-')}` : '';
     const yearQuery = years ? `&years=${years.join('-')}` : '';
-    const url = `${this.host}/api/search/v2/curriculum?q=${q}&size=${size}&from=${from}${schoolQuery}${yearQuery}`;
+    const url = `/api/search/v2/curriculum?q=${q}&size=${size}&from=${from}${schoolQuery}${yearQuery}`;
 
-    const response = await waitForFetch(
-      fetch(url, {
-        headers: this.authHeaders,
-      }),
-    );
+    const response = await this.fetch.getFromApiHost(url);
     const { results } = await response.json();
 
     return results;
@@ -45,13 +22,9 @@ export default class SearchService extends Service {
    */
   async forUsers(q, size = 100, onlySuggestEnabled = false) {
     const onlySuggest = onlySuggestEnabled ? '&onlySuggest=true' : '';
-    const url = `${this.host}/api/search/v1/users?q=${q}&size=${size}${onlySuggest}`;
+    const url = `/api/search/v1/users?q=${q}&size=${size}${onlySuggest}`;
 
-    const response = await waitForFetch(
-      fetch(url, {
-        headers: this.authHeaders,
-      }),
-    );
+    const response = await this.fetch.getFromApiHost(url);
     const { results } = await response.json();
 
     const { users, autocomplete } = results;
